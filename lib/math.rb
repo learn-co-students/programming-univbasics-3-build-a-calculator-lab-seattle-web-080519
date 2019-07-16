c4f054f0e92daa02f9b6e1d2d312f23ad8551204
Genevieve <kj.genevieve@gmail.com>
2019-07-16 02:34:42 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
+# addition - Build the method addition that adds num1 to num2 and returns the result of this calculation
 def addition(num1, num2)
-
+  sum = num1 + num2
+  p sum
 end
 
+# subtraction - Build the method subtraction that subtracts num1 from num2 and returns the result of this calculation
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  p difference
 end
 
+# division - Build the method division that divides num1 into num2 and returns the result of this calculation
 def division(num1, num2)
-
+  quotient = num1 / num2
+  p quotient
 end
 
+# multiplication - Build the method multiplication that multiplies num1 from num2 and returns the result of this calculation
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  p product
 end
 
+# modulo - Build the method modulo that divides num1 into num2 and gives us the remainder of this calculation
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  p remainder
 end
 
+# square_root -- Build the method square_root that finds the square root of num and returns the result
 def square_root(num)
-
+  result = Math.sqrt(num)
+  p result
 end
